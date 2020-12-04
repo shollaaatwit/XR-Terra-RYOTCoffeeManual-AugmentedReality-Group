1b9c56168b668091ba39cc0e77486e58ca3b2b47
shollaaatwit <55757460+shollaaatwit@users.noreply.github.com>
2020-12-03 20:31:40 -0500

    Sholla branch (#3)
    * add Hario Object
    
    * Update Readme
    
    * added Image Track
    
    * added more to ref library test
    
    Co-authored-by: gillysuit <contemporarygil@gmail.com>

---
+++ Assets/Sholla_Assets/Scripts/RecognitionTestingScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class RecognitionTestingScript : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImage;
+
+    private void Awake()
+    {
+        _arTrackedImage = FindObjectOfType<ARTrackedImageManager>();
+    }
+    public void OnEnable()
+    {
+        _arTrackedImage.trackedImagesChanged += OnImageChanged;
+    }
+    public void OnDisable()
+    {
+        _arTrackedImage.trackedImagesChanged -= OnImageChanged;
+    }
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added) //runs through all images
+        {
+            //do something
+        }
+    }
+}
