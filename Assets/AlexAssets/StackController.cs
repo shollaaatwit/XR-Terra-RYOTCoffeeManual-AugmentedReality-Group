953117c3e64bd1032dfbab42cf6f8b9b72bf66fa
LegioTitanicus <58441980+LegioTitanicus@users.noreply.github.com>
2020-12-06 18:15:34 -0500

    Adding assets (#5)
    * Created Coffee making stack (mug/hario/kettle/spoon)
    
    Added animation and particle effects for coffee grounds and steam fx. Commit is a WIP. As of now, stack will only place in AR if it's childed in a primitive with the mesh removed...
    
    * Updated-from-main

---
+++ Assets/AlexAssets/StackController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackController : MonoBehaviour
+{
+    public GameObject mug;
+    public GameObject hario;
+    public GameObject filter;
+    public GameObject spoon;
+    public GameObject kettle;
+    public ParticleSystem coffeeGrounds;
+    public ParticleSystem steam;
+
+    private Vector3 harioRestingPlace;
+    private Vector3 filterRestingPlace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowHarioPlacementAnimation()
+    {
+        //
+    }
+
+    public void SetHarioPlacement()
+    {
+        //
+    }
+
+    public void ShowFilterPlacementAnimation()
+    {
+        //
+    }
+
+    public void SetFilterPlacement()
+    {
+        //
+    }
+
+    public void PourCoffeeGroundsAnimation()
+    {
+        //
+    }
+
+    public void PourWaterAnimation()
+    {
+        //
+    }
+
+    public void UseSpoonToStirAnimation()
+    {
+        //
+    }
+}
