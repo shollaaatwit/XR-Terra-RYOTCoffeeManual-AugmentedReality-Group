ce0dd656e5bf6419542e466875a1cfac13c5e16c
Gilbert Ramirez <contemporarygil@gmail.com>
2020-12-06 14:52:30 -0800

    Add Basic UI Panels and canvas (#4)
    * Add Fonts and basic Welcome and Size selection panels
    
    * Add basic UI layout

---
+++ Assets/Gil_Assets/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    
+    public Text timerText;
+    public TMP_Text buttonText;
+    public bool isTimerOn = false;
+    private float theTime = 0;
+    private string _initialButtonText;
+    [SerializeField] private float _speed;
+    void Start()
+    {
+        _speed = 1f;
+        _initialButtonText = buttonText.text;
+    }
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        if (isTimerOn)
+        {
+            theTime += Time.deltaTime * _speed;
+            string minutes = Math.Floor((theTime % 3600) / 60).ToString("00");
+            string seconds = (theTime % 60).ToString("00");
+
+            timerText.text = minutes + ":" + seconds;
+        }
+    }
+
+    public void HandleTimerButton()
+    {
+        
+        if (!timerText.isActiveAndEnabled)
+        {
+            timerText.gameObject.SetActive(true); 
+        }
+        isTimerOn = !isTimerOn;
+
+        buttonText.text = isTimerOn ? "Pause" : "Resume";
+    }
+
+    private void ResetTimer()
+    {
+        theTime = 0f;
+        isTimerOn = false;
+        buttonText.text = _initialButtonText;
+        _speed = 1f;
+    }
+
+}
