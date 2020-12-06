a0b052fe918e7b81fe9108c754af75c64938be47
LegioTitanicus <alexbernstein7170@gmail.com>
2020-12-06 17:53:47 -0500

    Created Coffee making stack (mug/hario/kettle/spoon)
    Added animation and particle effects for coffee grounds and steam fx. Commit is a WIP. As of now, stack will only place in AR if it's childed in a primitive with the mesh removed...

---
+++ Assets/AlexAssets/CircleKettle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+//Attach this script to a GameObject to rotate around the target position.
+public class CircleKettle : MonoBehaviour
+{
+    //Assign a GameObject in the Inspector to rotate around
+    public GameObject target;
+
+    void Update()
+    {
+        // Spin the object around the target at 20 degrees/second.
+        transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
